bda296762913f1fe877b9fbbbe3a8e68709222d9
fuomag <fuomag@gmail.com>
2021-10-18 18:20:52 +0200

    avoid app crashing if saving screenshot without video opened

--- app/src/main/java/com/fuomag9/videoscreenshotter/MainActivity.kt
+++ app/src/main/java/com/fuomag9/videoscreenshotter/MainActivity.kt
@@ -37,9 +37,7 @@ import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
 import android.content.Intent
-
-
-
+import android.widget.Toast
 
 
 class MainActivity : ComponentActivity() {
@@ -118,10 +116,16 @@ private fun OpenDocumentPicker() {
         //Todo: get recycle video filename here
         Button(
             onClick = {
-                val documentFile = DocumentFile.fromSingleUri(context, MainActivity.Uri.value!!)
-                var filename = documentFile!!.name?.substringBeforeLast(".")
-                filename += "-" + MainActivity.exoplayer.currentPosition.toString() + ".png";
-                launchercreate.launch(filename)
+                try {
+                    val documentFile = DocumentFile.fromSingleUri(context, MainActivity.Uri.value!!)
+                    var filename = documentFile!!.name?.substringBeforeLast(".")
+                    filename += "-" + MainActivity.exoplayer.currentPosition.toString() + ".png";
+                    launchercreate.launch(filename)
+                }
+                catch (e: Exception){
+                    Toast.makeText(context,"You probably did not open a video",Toast.LENGTH_SHORT).show()
+                }
+
             },
             modifier = Modifier.padding(top = 20.dp)
         ) {
